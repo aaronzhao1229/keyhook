2f32af30f55edfab8a1b64db58c347d1abff62a3
Aaron Zhao <aaron@pivotpoint.co.nz>
2024-10-30 10:05:35 +1300

    add manual sorting state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import EmployeeTable from "./components/EmployeeTable"
+import EmployeeTable from './components/EmployeeTable'
 
 const App = () => {
   return (
     <>
-      <h1 className="text-2xl font-bold underline">Keyhook Test Project</h1>
+      <h1 className="text-2xl font-bold underline mt-8 flex justify-center">
+        Keyhook Test Project
+      </h1>
       <EmployeeTable />
     </>
   )
--- frontend/src/components/EmployeeTable.tsx
+++ frontend/src/components/EmployeeTable.tsx
@@ -1,63 +1,90 @@
-import React, { useMemo, useEffect, useState } from 'react';
-import { useReactTable, getCoreRowModel, flexRender, SortingState, PaginationState } from '@tanstack/react-table';
+import React, { useMemo, useEffect, useState } from 'react'
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  SortingState,
+  PaginationState,
+} from '@tanstack/react-table'
 
-import { apiResponse, Employee, EmployeeParams } from '../models/employee';
-import agent from '../api/agent';
-import { Filter } from './Filter';
-import { getAxiosParams } from '../api/helper';
-import { ArrowsUpDownIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
-import { Department, DepartmentIdAndName } from '../models/department';
-import { Dropdown } from './Dropdown';
+import { apiResponse, Employee, EmployeeParams } from '../models/employee'
+import agent from '../api/agent'
+import { Filter } from './Filter'
+import { getAxiosParams } from '../api/helper'
+import {
+  ArrowsUpDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from '@heroicons/react/24/outline'
+import { Department, DepartmentIdAndName } from '../models/department'
+import { Dropdown } from './Dropdown'
 
 const EmployeeTable: React.FC = () => {
-  const [data, setData] = useState<Employee[]>([]);
-  const [departments, setDepartments] = useState<DepartmentIdAndName[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<Employee[]>([])
+  const [departments, setDepartments] = useState<DepartmentIdAndName[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
   const [sorting, setSorting] = useState<SortingState>([])
-  const [pagination, setPagination] = useState<PaginationState>({ pageIndex: 0, pageSize: 20 })
-  
-  const [employeePramas, setEmployeePramas] = useState<EmployeeParams>({include: 'department'});
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 20,
+  })
+
+  const [employeePramas, setEmployeePramas] = useState<EmployeeParams>({
+    include: 'department',
+  })
 
   // fetch employees data with params
   const fetchEmployees = async (params: EmployeeParams) => {
-      const axiosParams = getAxiosParams(params);
-      return agent.Employees.getEmployees(axiosParams)
-        .then((employeesData) => {
-          const employees = employeesData.data.map((employee: apiResponse) => employee.attributes);
-          setData(employees);
-          return employees
-        })
-        .catch((error) => {
-          setError(error.message);
-        });
-    };
+    const axiosParams = getAxiosParams(params)
+    return agent.Employees.getEmployees(axiosParams)
+      .then((employeesData) => {
+        const employees = employeesData.data.map(
+          (employee: apiResponse) => employee.attributes
+        )
+        setData(employees)
+        return employees
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+  }
 
-    const fetchDepartments =  () => {
-      
-      agent.Departments.getDepartments()
-        .then((departmentsData) => {
-          const departmentIdsAndNames : DepartmentIdAndName[] = departmentsData.data.map((department: Department) => {return {id: department.id, name: department.attributes.name}});
-          setDepartments(departmentIdsAndNames);
-        })
-        .catch((error) => {
-          setError(error.message);
-        });
-    };
-  
+  const fetchDepartments = () => {
+    agent.Departments.getDepartments()
+      .then((departmentsData) => {
+        const departmentIdsAndNames: DepartmentIdAndName[] =
+          departmentsData.data.map((department: Department) => {
+            return { id: department.id, name: department.attributes.name }
+          })
+        setDepartments(departmentIdsAndNames)
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+  }
+
   // handle search by name
-  const handleFilterChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setEmployeePramas(prev => ({...prev, 'filter[name]': e.target.value}))
-    fetchEmployees({...employeePramas, 'filter[name]': e.target.value})
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmployeePramas((prev) => ({ ...prev, 'filter[name]': e.target.value }))
+    fetchEmployees({ ...employeePramas, 'filter[name]': e.target.value })
   }
 
   // handle filter by department
-  const handleSelectedDepartmentChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectedDepartmentChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     if (e.target.value !== '0') {
-    setEmployeePramas(prev => ({...prev, 'filter[department_id]': e.target.value}))
-    fetchEmployees({...employeePramas, 'filter[department_id]': e.target.value})
+      setEmployeePramas((prev) => ({
+        ...prev,
+        'filter[department_id]': e.target.value,
+      }))
+      fetchEmployees({
+        ...employeePramas,
+        'filter[department_id]': e.target.value,
+      })
     } else {
-      const newState = {...employeePramas}
+      const newState = { ...employeePramas }
       delete newState['filter[department_id]']
       setEmployeePramas(newState)
       fetchEmployees(newState)
@@ -65,14 +92,13 @@ const EmployeeTable: React.FC = () => {
   }
 
   useEffect(() => {
-    fetchEmployees({include: 'department'})
+    fetchEmployees({ include: 'department' })
     fetchDepartments()
     setLoading(false)
-  }, []);
+  }, [])
 
-  
   // define columns
-    const columns = useMemo(
+  const columns = useMemo(
     () => [
       {
         accessorKey: 'first_name',
@@ -98,83 +124,103 @@ const EmployeeTable: React.FC = () => {
         accessorKey: 'department_name',
         header: 'Department',
         cell: (props) => <p>{props.getValue()}</p>,
-        enableSorting: false
+        enableSorting: false,
       },
     ],
     []
-  );
-  
+  )
+
   const table = useReactTable({
     data,
     columns,
-    state:{
+    state: {
       sorting,
-      pagination
+      pagination,
     },
     getCoreRowModel: getCoreRowModel(),
     manualPagination: true,
-    pageCount:50,
+    manualSorting: true,
+    pageCount: 50,
     onSortingChange: setSorting,
     onPaginationChange: setPagination,
-     })
+  })
 
   useEffect(() => {
-    // sorting 
+    // sorting
     if (table.getState().sorting.length > 0) {
-      const sortId = table.getState().sorting[0].desc ? '-' + table.getState().sorting[0].id : table.getState().sorting[0].id
-      setEmployeePramas(prev => ({...prev, sort: sortId}))
-      fetchEmployees({...employeePramas, sort: sortId})
-    }
-    else {
-      const {sort, ...newValues} = employeePramas
+      const sortId = table.getState().sorting[0].desc
+        ? '-' + table.getState().sorting[0].id
+        : table.getState().sorting[0].id
+      setEmployeePramas((prev) => ({ ...prev, sort: sortId }))
+      fetchEmployees({ ...employeePramas, sort: sortId })
+    } else {
+      const { sort, ...newValues } = employeePramas
       setEmployeePramas(newValues)
       fetchEmployees(newValues)
     }
-    }, [table.getState().sorting]);
+  }, [table.getState().sorting])
 
   useEffect(() => {
     // pagination
     const pageIndex = table.getState().pagination.pageIndex + 1
     if (pageIndex !== employeePramas['page[number]']) {
-      setEmployeePramas(prev => ({...prev, 'page[number]': pageIndex}))
-      fetchEmployees({...employeePramas, 'page[number]': pageIndex})
+      setEmployeePramas((prev) => ({ ...prev, 'page[number]': pageIndex }))
+      fetchEmployees({ ...employeePramas, 'page[number]': pageIndex })
     }
-    }, [table.getState().pagination.pageIndex]);
-  
+  }, [table.getState().pagination.pageIndex])
+
   if (loading) {
-    return <div className='flex justify-center mt-16'>Loading...</div>;
+    return <div className="flex justify-center mt-16">Loading...</div>
   }
 
   if (error) {
-    return <div className='flex justify-center mt-16'>{error} {'. Please contact the developer.'}</div>;
+    return (
+      <div className="flex justify-center mt-16">
+        {error} {'. Please contact the developer.'}
+      </div>
+    )
   }
-  
+
   return (
     <div className="px-16 py-8 w-full flex flex-col">
-      <div className='flex justify-left'>
-        <Filter employeePramas={employeePramas} handleFilterChange={handleFilterChange}/>
-        <Dropdown options={[{id: '0', name: 'Filter by department'}, ...departments]}  handleSelectChange={handleSelectedDepartmentChange}/>
+      <div className="flex justify-left">
+        <Filter
+          employeePramas={employeePramas}
+          handleFilterChange={handleFilterChange}
+        />
+        <Dropdown
+          options={[{ id: '0', name: 'Filter by department' }, ...departments]}
+          handleSelectChange={handleSelectedDepartmentChange}
+        />
       </div>
-      
-      <table className='table-auto border-collapse border border-slate-400'>
+
+      <table className="table-auto border-collapse border border-slate-400">
         <thead>
-          {table.getHeaderGroups().map(headerGroup => (
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
-                <th key={header.id} className='border border-slate-300 bg-slate-100 py-2'>
-                  <div className='flex items-center justify-center'>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                      {header.column.getCanSort() && <ArrowsUpDownIcon className={'mx-2 hover:cursor-pointer text-slate-500'} width={20} onClick={header.column.getToggleSortingHandler()
-                      }/>}
-                      {{
-                          asc: ' 🔼',
-                          desc: ' 🔽',
-                        }[header.column.getIsSorted() as string] ?? null}
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="border border-slate-300 bg-slate-100 py-2"
+                >
+                  <div className="flex items-center justify-center">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    {header.column.getCanSort() && (
+                      <ArrowsUpDownIcon
+                        className={'mx-2 hover:cursor-pointer text-slate-500'}
+                        width={20}
+                        onClick={header.column.getToggleSortingHandler()}
+                      />
+                    )}
+                    {{
+                      asc: ' 🔼',
+                      desc: ' 🔽',
+                    }[header.column.getIsSorted() as string] ?? null}
                   </div>
                 </th>
               ))}
@@ -182,11 +228,13 @@ const EmployeeTable: React.FC = () => {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => (
+          {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id} className='border border-slate-300'>
-                  <div className='flex items-center justify-center'>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} className="border border-slate-300">
+                  <div className="flex items-center justify-center">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </div>
                 </td>
               ))}
             </tr>
@@ -195,16 +243,28 @@ const EmployeeTable: React.FC = () => {
       </table>
 
       {/* pagination */}
-      <div className='my-4'>
-        <p className='text-sm mb-2'>
-        Page {' '} {table.getState().pagination.pageIndex + 1} of {' '} {table.getPageCount()}
+      <div className="my-4">
+        <p className="text-sm mb-2">
+          Page {table.getState().pagination.pageIndex + 1} of{' '}
+          {table.getPageCount()}
         </p>
-        <button className='mr-2 border border-slate-300 rounded hover:cursor-pointer hover:bg-slate-200' onClick={table.previousPage} disabled={!table.getCanPreviousPage()}><ChevronLeftIcon width={20}/></button>
-        <button className='mr-2 border border-slate-300 rounded hover:cursor-pointer hover:bg-slate-200' onClick={table.nextPage} disabled={!table.getCanNextPage()}><ChevronRightIcon width={20}/></button>
+        <button
+          className="mr-2 border border-slate-300 rounded hover:cursor-pointer hover:bg-slate-200"
+          onClick={table.previousPage}
+          disabled={!table.getCanPreviousPage()}
+        >
+          <ChevronLeftIcon width={20} />
+        </button>
+        <button
+          className="mr-2 border border-slate-300 rounded hover:cursor-pointer hover:bg-slate-200"
+          onClick={table.nextPage}
+          disabled={!table.getCanNextPage()}
+        >
+          <ChevronRightIcon width={20} />
+        </button>
       </div>
-      
     </div>
-  );
-};
+  )
+}
 
-export default EmployeeTable;
+export default EmployeeTable
