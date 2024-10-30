8a15ecc532ed1ccc5817b3b9f8b278df3ce4c2bc
Aaron Zhao <aaron@pivotpoint.co.nz>
2024-10-30 13:20:29 +1300

    adjust styles

--- frontend/src/components/AddEmployeeModal.tsx
+++ frontend/src/components/AddEmployeeModal.tsx
@@ -2,27 +2,25 @@ export default function AddEmployeeModal() {
   return (
     <>
       <button
-        className="mr-2 border border-slate-300 rounded hover:cursor-pointer hover:bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="btn"
         onClick={() =>
           (
-            document.getElementById('my_modal_1') as HTMLDialogElement
+            document.getElementById('my_modal_3') as HTMLDialogElement
           ).showModal()
         }
       >
-        Add a new employee
+        open modal
       </button>
-      <dialog id="my_modal_1" className="modal">
+      <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
           <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
-          </div>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
         </div>
       </dialog>
     </>
